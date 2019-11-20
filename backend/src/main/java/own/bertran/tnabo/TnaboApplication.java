aa10b1d271b439a2a945004f80e0b5506623897e
bertranwang <bowdenwong@outlook.com>
2019-11-20 21:44:10 +0800

    init backend

---
+++ backend/src/main/java/own/bertran/tnabo/TnaboApplication.java
@@ -0,0 +1,13 @@
+package own.bertran.tnabo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TnaboApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TnaboApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/own/bertran/tnabo/TnaboApplicationTests.java
@@ -0,0 +1,13 @@
+package own.bertran.tnabo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TnaboApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
